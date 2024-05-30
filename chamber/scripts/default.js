86738d14e08e07c783af6166a85d12707858a736
PineappleLord2020 <mul20002@byui.edu>
2024-05-30 12:06:26 -0600

    The Hero Image was the twist Villain

--- chamber/scripts/default.js
+++ chamber/scripts/default.js
@@ -9,3 +9,26 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+/*function getMessage() {
+	let currentDate = new Date();
+	let date = localStorage.getItem('date');
+
+	if (date) {
+		storeDate = new Date(storeDate);
+	}
+
+	if (date) {
+		let difference = currentDate.getMilliseconds - storeDate.getMilliseconds;
+		
+	} else{
+		message = "Welcome! Let us know if you have any questions."
+	}
+
+	localStorage.setItem('date', currentDate.getTime());
+
+	return message;
+}
+
+let displayMessage = getMessage();
+document.querySelector('#message').textContent = displayMessage;*/
--- scripts/main.js
+++ scripts/main.js
@@ -24,3 +24,20 @@ modeButton.addEventListener("click", () => {
 		modeButton.textContent = "🕶️";
 	}
 });
+
+function getVisitCount() {
+	let count = localStorage.getItem('visitCount');
+
+	if (count) {
+		count++;
+	} else {
+		count = 1;
+	}
+
+	localStorage.setItem('visitCount', count);
+
+	return count;
+}
+
+let visitCount = getVisitCount();
+document.querySelector('#count').textContent = visitCount;
