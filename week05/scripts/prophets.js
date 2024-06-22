8d1bf5fcedf921f77857a18a0034264262f7fba9
PineappleLord2020 <mul20002@byui.edu>
2024-06-22 08:19:46 -0600

    Worked with some Javascript this week

---
+++ week05/scripts/prophets.js
@@ -0,0 +1,43 @@
+const url= 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+
+const cards = document.querySelector('#cards');
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    displayProphets(data.prophets);
+
+    //console.table(data.prophets);
+}
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        const card = document.createElement('section');
+        const fullName = document.createElement('h2');
+        const portrait = document.createElement('img');
+        const dateOfBirth = document.createElement('p');
+        const placeOfBirth = document.createElement('p')
+
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        dateOfBirth.textContent = `Date of Birth: ${prophet.birthdate}`
+
+        placeOfBirth.textContent = `Place of Birth: ${prophet.birthplace}`
+
+        card.appendChild(fullName);
+        card.appendChild(dateOfBirth);
+        card.appendChild(placeOfBirth);
+        card.appendChild(portrait);
+        cards.appendChild(card);
+
+    })
+}
+
+getProphetData();
