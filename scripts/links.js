a830683e6dd604c2d6ddcf5ff326a434191c704e
PineappleLord2020 <mul20002@byui.edu>
2024-06-25 11:06:37 -0600

    Did a lot of JSON editing

---
+++ scripts/links.js
@@ -0,0 +1,35 @@
+//const baseURL = 'https://https://pineapplelord2020.github.io/wdd230/';
+//const linksURL= 'https://https://pineapplelord2020.github.io/wdd230/data/links.json';
+
+const linksURL = "../data/links.json";
+
+const links = document.querySelector("#assignmentLinks");
+
+async function getAssignments() {
+  const response = await fetch(linksURL);
+  const data = await response.json();
+
+  displayAssignments(data);
+
+  //console.table(data.prophets);
+}
+
+const displayAssignments = (assignments) => {
+  assignments.forEach((assignment) => {
+    const week = document.createElement("p");
+    week.textContent = assignment.week;
+
+    console.log(assignment);
+    links.appendChild(week);
+
+    assignment.links.forEach((link) => {
+      const linkElement = document.createElement("a");
+      linkElement.textContent = link.title;
+      linkElement.setAttribute("href", link.url);
+      links.appendChild(linkElement);
+    });
+
+  });
+};
+
+getAssignments();
--- scripts/main.js
+++ scripts/main.js
@@ -41,3 +41,36 @@ function getVisitCount() {
 
 let visitCount = getVisitCount();
 document.querySelector('#count').textContent = visitCount;
+
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=42.55573883526419&lon=-114.47048271174137&appid=70acdbc7066a8c4ce4c025b90a2fa3b0&units=imperial'
+
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+apiFetch();
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp.toFixed(0)}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+}
