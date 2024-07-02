8220521c0c8c7b4ac3b147a55cb5cce672a77f85
PineappleLord2020 <mul20002@byui.edu>
2024-07-02 11:04:50 -0600

    Made some API Changes to the Chamber site

--- chamber/scripts/default.js
+++ chamber/scripts/default.js
@@ -40,6 +40,89 @@ function getMessage() {
 	return message;
 }
 
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('#captionDesc');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=34.36597806132115&lon=-89.51930703708217&appid=70acdbc7066a8c4ce4c025b90a2fa3b0&units=imperial'
+
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+apiFetch();
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp.toFixed(0)}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+}
+
+/*const futureTemp = document.querySelector('#future-temp');
+const weatherWeekIcon = document.querySelector('#weather-week-icon');
+const captionForcastDesc = document.querySelector('#forcastDesc');
+
+const fiveDayURL = 'api.openweathermap.org/data/2.5/forecast?lat=34.36597806132115&lon=-89.51930703708217&appid=70acdbc7066a8c4ce4c025b90a2fa3b0&units=imperial'
+const futureForcast = document.querySelector("#weatherForcast")
+
+async function FiveDayFetch() {
+    try {
+        const response = await fetch(fiveDayURL);
+        if (response.ok) {
+            const dataWeek = await response.json();
+            console.log(dataWeek);
+            displayResults(dataWeek);
+        } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+FiveDayFetch();
+
+
+
+function displayResults = (results) => {
+	results.forEach((result) => {
+		const temperature = document.createElement("span");
+		futureForcast.appendChild(temperature);
+
+		results.list.forEach((listElement) => {
+			const weekTemp = document.createElement("p");
+			weekTemp.textContent = listElement.main.temp;
+			const weekDescription = document.createElement("figcaption");
+			weekDescription = listElement.weather.main;
+			futureForcast.appendChild(weekTemp);
+		})
+	})
+}
+	
+    futureTemp.innerHTML = `${dataWeek.main.temp.toFixed(0)}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${dataWeek.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+}*/
+
+
 let displayMessage = getMessage();
 document.querySelector('#message').textContent = displayMessage;
 
