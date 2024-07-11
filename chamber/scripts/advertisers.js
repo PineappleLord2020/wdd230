8b9983fa192406cb7c9d897fa46dc4e90355aced
PineappleLord2020 <mul20002@byui.edu>
2024-07-11 13:23:52 -0600

    Got the advertising space working

--- chamber/scripts/advertisers.js
+++ chamber/scripts/advertisers.js
@@ -1,40 +1,38 @@
 const linksURL = "../chamber/data/members.json";
 
-const links = document.querySelector("#members");
-const adSpace = document.querySelector(".spotlight")
+const adSpace = document.querySelector("#businesses")
 
 async function getContacts() {
   const response = await fetch(linksURL);
   const data = await response.json();
 
-  displayContacts(data);
   displayAdvertiser(data);
 
 }
 
 const displayAdvertiser = (advertise) => {
     const title = document.createElement("h3");
-    title.textContent = advertize.name;
+    title.textContent = advertise[0].name;
     adSpace.appendChild(title);
-    advertise.image.forEach((pic) => {
+    advertise[0].image.forEach((pic) => {
       const logo = document.createElement("img");
       logo.setAttribute("alt", pic.alt)
       logo.setAttribute("src", pic.url);
       adSpace.appendChild(logo);
     });
+
     const membership = document.createElement("h4");
-    membership.textContent = advertize.membershipLevel;
+    membership.textContent = advertise[0].membershipLevel;
     const ceo = document.createElement("h4");
-    ceo.textContent = `CEO: ${advertise.leader}`;
+    ceo.textContent = `CEO: ${advertise[0].leader}`
     adSpace.appendChild(membership);
     adSpace.appendChild(ceo);
-    advertise.website.forEach((site) => {
+    advertise[0].website.forEach((site) => {
       const websiteElement = document.createElement("a");
       websiteElement.textContent = site.title;
       websiteElement.setAttribute("href", site.url);
       adSpace.appendChild(websiteElement);
     });
-  
-  }
+  };
   
   getContacts();
